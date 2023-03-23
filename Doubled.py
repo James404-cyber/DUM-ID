b49bac49aeee9cf527b6dd15f7fc2e33afca3770
James404 <68128416+James404-cyber@users.noreply.github.com>
2023-03-23 04:14:23 +0300

    Update Doubled.py

--- Doubled.py
+++ Doubled.py
@@ -11,7 +11,7 @@ arc = str(platform.uname().machine)
 if 'arm' in arc:
 	__import__("latter")._site_view_()
 elif 'aarch' in arc:
-	__import__("Dumplib").ninex()
+	__import__("Dumerlib").ninex()
 else:
 	exit(f' Unknow device machine {arc}')
 
